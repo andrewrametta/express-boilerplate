1577341598986a0678288a3f4aef9b460c13b9e4
andrewrametta <73006425+andrewrametta@users.noreply.github.com>
2021-02-02 13:07:15 -0500

    Update app.js
    fixed morganOption

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOption = (NODE_ENV = "production" ? "tiny" : "common");
+const morganOption = (NODE_ENV === "production" ? "tiny" : "common");
 
 app.use(morgan(morganOption));
 app.use(cors());
